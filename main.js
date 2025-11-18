178a2be3cd18c0561dd7bfffa91124940cb99763
Victor Vitali <victorvitali@gmail.com>
2025-11-18 20:39:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 usuarios = ['teste'];
 senhas = ['123'];
-tipos = ['tec'];
+cargos = ['tec'];
 
 
 function SelectFiltroDMA(Selected) {
@@ -9,7 +9,6 @@ function SelectFiltroDMA(Selected) {
     //VAI ADCIONAR ACTIVE NO SELECIONADO
     console.log(Selected);
     document.querySelector("#filtroDMA ." + Selected).classList.add('active');
-
 
 }
 function CriarNotificacao(tipo = "Erro", mensagem = "Função não implementada ainda (Fui jogar lol)") {
@@ -29,52 +28,19 @@ function CriarNotificacao(tipo = "Erro", mensagem = "Função não implementada 
 function CloseAlert() {
     document.getElementById("Alert").style.display = ("none");
 }
-function carregaUsuarios() {
-    // Essa é a funcao mais dificil do site, entao vou explicar quase linha por linha
-    //Isso le o arquivo, mas como nao quero usar funcao assincrona, vou usar o Then
-    //oq o Then faz: Ele só faz aquilo que ta nele, DEPOIS de ter feito oq esta antes, sem isso, ele ia "Vou ler o arquivo" e ja ia pra proxima parte
-    //Oq ocasionaria de ele tentar executar a proxima parte do codigo sem nem ter retornado com oq foi lá ler no arquivo.
+function criarCadastro(){
+    var nome = document.getElementById('emailR').value;
+    var senha = document.getElementById('passwordR').value;
+    var tipo = document.getElementById("tipoR").value;
 
-    //Basicamente:
-    // var texto = fetch('usuarios.txt'); // Me da o arquivo AGORA
-    // console.log(texto); // vai dar Undefined pq o arquivo ainda não deu tempo de ler
-
-    //OBS que descobri só hoje: o fetch nao funciona se não tiver um servidor (o chrome bloqueia de ler o arquivo)...
-    //entao tem que usar a extensão Live Server xD
-    fetch('./usuarios.txt')
-        .then(response => response.text()) //response é oq veio do fetch, aqui ele vai transformar em um texto oq veio do fetch
-        .then(texto => {
-
-            // Divide por linhas
-            const linhas = texto.split('\n');
-
-            linhas.forEach(linha => {
-                linha = linha.trim();
-
-                const partes = linha.split(':');
-                const usuario = partes[0].trim();
-                const senha = partes[1].trim();
-                const tipo = partes[2].trim();
-
-                if (!usuarios.indexOf(usuario)) { //Se não existir no array ainda ele vai colocar
-                    //O push ele insere na ultima posicao do array
-                    usuarios.push(usuario);
-                    senhas.push(senha);
-                    tipos.push(tipo);
-                }
-            });
-            return true;
-        })
-        .catch(error => { //Isso eu vou tirar no final é apenas para me ajudar, menos uma coisa para eu ter q explicar
-            console.log("Deu ruim, vai olhar o console");
-            return false;
-        });
+    usuarios.push(nome);
+    senhas.push(senha);
+    cargos.push(tipo);
 }
-
 
 function switchLoginRegistro() {
     var divs = document.getElementsByClassName("login-card");
-    
+    console.log('pq ta entrando aqui???');
     for (var i = 0; i < divs.length; i++) { //Faz a funcao igual um forEach, mas aqui usa for, entao vai percorrer X vezes sendo X a quantidade de elementos com esse classe.
         var div = divs[i];
         var displayAtual = div.style.display;     //Pega como está o display desse element
@@ -87,7 +53,7 @@ function switchLoginRegistro() {
 }
 
 function changeAba(click){
-    abas = ['banco', 'historico'];
+    abas = ['banco', 'historico', 'solicitar', 'pedidos', 'relatorio'];
     abas.forEach(aba => {
         var div = document.querySelector("."+aba+"-container");
         if (click == aba) {
